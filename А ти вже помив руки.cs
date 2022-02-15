788d515f47587627be4ba16ac7db4d1618b17089
Igor Kachynskyy <35116904+igorkachynskyy@users.noreply.github.com>
2022-02-15 16:40:20 +0200

    Create А ти вже помив руки.cs

---
+++ А ти вже помив руки.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine().Split(' ');
+            int a = Int32.Parse(s[0]);
+            int b = Int32.Parse(s[1]);
+            int k = Int32.Parse(s[2]);
+            int sum = a;
+            for (int i = 0; i < k;i++ )
+            {
+                sum += b;
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
+
